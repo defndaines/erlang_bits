990e434e8e91f385cd9516c2a7bdb6fd5d53d9b0
defndaines <defndaines@gmail.com>
2015-06-27 03:46:34 -0700

    Short-circuiting filter function.

--- src/coll.erl
+++ src/coll.erl
@@ -3,12 +3,36 @@
 %%% Collection functions, some inspired by Clojure.
 
 -module(coll).
--export([frequencies/1]).
+-export([filter/3, frequencies/1]).
 
 -type freq_list(T) :: proplists:proplist({T, non_neg_integer()}).
 
+%% Short-circuiting filter. For use when `List' is rather long and only `Len'
+%% elements are needed.
+%%
+%% TODO: Establish a rough guideline of when this is helpful or more performant.
+-spec filter(fun((Elem :: T) -> boolean()), list(T), pos_integer()) -> list(T).
+filter(_Pred, [], _Len) -> [];
+filter(Pred, List, Len) ->
+  ShortCircuitFn = fun
+                     (_, Acc) when length(Acc) >= Len -> throw({enough, Acc});
+                     (Elem, Acc) ->
+                       case Pred(Elem) of
+                         true -> [Elem | Acc];
+                         false -> Acc
+                       end
+                   end,
+  Matching = try
+               lists:foldl(ShortCircuitFn, [], List)
+             catch
+               {enough, Acc} -> Acc
+             end,
+  lists:reverse(Matching).
+
 %% Returns a proplist of distinct items in a list to the number of times
 %% they appear.
+%%
+%% NOTE: There is a more native way to do this.
 %%
 %% Examples:
 %% > coll:frequencies([red, blue, green, green, red, red, green, red, green]).
